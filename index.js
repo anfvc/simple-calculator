0a78004d901926b250cde3bb029dfa4026c8abbb
andres <andres.villay@dci-student.org>
2024-06-04 21:32:56 +0200

    adding functions

---
+++ index.js
@@ -0,0 +1,39 @@
+const display = document.querySelector(".display");
+console.log(display);
+
+function tapButton(character) {
+
+  if (display.value === "0") {
+    display.value = character
+  } else {
+    display.value += character;
+
+  }
+
+}
+
+function clearDisplay() {
+  display.value = "0";
+}
+
+function total() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Syntax Error";
+  }
+}
+
+function sum(value) {
+  display.value + value;
+}
+
+function deleteChar() {
+  display.value = display.value.slice(0, -1);
+}
+
+display.addEventListener("input", () => {
+  if (display.value === "0") {
+    display.value = "";
+  }
+})
